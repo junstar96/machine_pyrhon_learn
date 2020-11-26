1704df3a7bb864a3ee3cd0d1acb5a255871de023
jemuras <junstar96@naver.com>
2020-11-27 01:57:19 +0900

    streamming test

--- push_test.py
+++ push_test.py
@@ -1,7 +1,25 @@
-import sys
-import numpy
+import urllib.request
+import requests, io, os
+import numpy as np
+import tarfile, zipfile, gzip
 
-print("hello world")
+def unzip_from_UCI(UCI_url, dest=''):
+    #ucl의 데이터 세트를 zip 포맷으로 다운로드하고 압축을 푼다.
+    response = requests.get(UCI_url)
+    compressed_file = io.BytesIO(response.content)
+    z = zipfile.ZipFile(compressed_file)
+    print('extracting in %s' % os.getcwd()+'\\'+dest)
+    for name in z.namelist():
+        if '.csv' in name:
+            print('tunxipping %s' %name)
+            z.extract(name, path=os.getcwd()+'\\'+dest)
+
+
+def load_matrix(UCI_url):
+    #uci의 데이터세트를 행렬 포맷으로 다운로드하고 압축을 푼다.
+    return np.loadtxt(urllib.request.urlopen(UCI_url))
+
+
 
 
 #what the hack?
